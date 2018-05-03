e9b0ed0d5115410010aebad2f9e7241ab5de83db
df <kdfmail@qq.com>
2018-05-03 09:28:53 +0800

    a

---
+++ golang.org/x/crypto/ssh/tool_test.go
@@ -0,0 +1,51 @@
+package gosshtool
+
+import (
+	"testing"
+)
+
+func Test_newSSHClinet(t *testing.T) {
+	/*	config := &SSHClientConfig{
+			User:     "s",
+			Password: "1223",
+			Host:     "127.0.0.1",
+		}
+		sshclient := NewSSHClient(config)
+		stdout, stderr,session, err := sshclient.Cmd("pwd",nil,nil,0)
+		if err != nil {
+			t.Error(err)
+		}
+		t.Log(stdout)
+		t.Log(stderr)
+		stdout, stderr,session, err = sshclient.Cmd("ls",nil,nil,0)
+		t.Log(stdout)
+		t.Log("test")*/
+}
+
+func Test_mutiCmd(t *testing.T) {
+	/*	config := &SSHClientConfig{
+			User:     "jack",
+			Password: "assd",
+			Host:     "31.11.11.11",
+		}
+		NewSSHClient(config)
+
+		config2 := &SSHClientConfig{
+			User:     "asd",
+			Password: "daas",
+			Host:     "8.8.8.8",
+		}
+		NewSSHClient(config2)
+		stdout, _, _,err := ExecuteCmd("pwd", "8.8.8.8")
+		if err != nil {
+			t.Error(err)
+		}
+		t.Log(stdout)
+
+		stdout, _,_, err = ExecuteCmd("pwd", "114.215.151.48")
+		if err != nil {
+			t.Error(err)
+		}
+		t.Log(stdout)*/
+	t.Log("test")
+}
